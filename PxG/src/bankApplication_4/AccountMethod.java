96e7e0789bd26a0bade218bde71941a07841d348
Daehyeon Hong <fholng@outlook.com>
2020-06-15 00:46:07 +0900

    수정사항 업데이트

--- PxG/src/bankApplication_4/AccountMethod.java
+++ PxG/src/bankApplication_4/AccountMethod.java
@@ -77,8 +77,8 @@ public class AccountMethod {
 		int f = 10000, m = 1000, l = 100000;
 		while (true) {
 			int fir = (random.nextInt(f) + (f / 10));
-			int mid = random.nextInt(m) + (m / 10);
-			int last = random.nextInt(l) + (l / 10);
+			int mid = (random.nextInt(m) + (m / 10));
+			int last = (random.nextInt(l) + (l / 10));
 			if (fir < f && mid < m && last < l) {
 				ano = (fir + "-" + mid + "-" + last);
 				System.out.println(ano);
